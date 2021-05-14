039ac5f8283613308a86e77dbaad602dafae6a5e
Carlos San Bartolomé <carlos.sanbs@gmail.com>
2021-05-14 17:24:35 -0500

    DONE add scully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
     CoreModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    ScullyLibModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
