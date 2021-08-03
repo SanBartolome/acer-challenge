4e8350ac10b10243356ad58f172f8ddedccd2cbc
Carlos San Bartolomé <carlos.sanbs@gmail.com>
2021-08-03 02:45:42 -0500

    DONE add fadein and fadeout animation

--- src/app/pages/home/components/second-section/second-section.component.ts
+++ src/app/pages/home/components/second-section/second-section.component.ts
@@ -1,13 +1,58 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ElementRef,
+  HostListener,
+} from '@angular/core';
 
 @Component({
   selector: 'app-second-section',
   templateUrl: './second-section.component.html',
   styleUrls: ['./second-section.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('scrollAnimation', [
+      state(
+        'scrolled',
+        style({
+          opacity: 1,
+        })
+      ),
+      state(
+        'normal',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition('scrolled => normal', animate('500ms ease-out')),
+      transition('normal => scrolled', animate('250ms ease-in')),
+    ]),
+  ],
 })
 export class SecondSectionComponent implements OnInit {
-  constructor() {}
+  state = 'normal';
+
+  constructor(public el: ElementRef) {}
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll(): void {
+    const componentPosition = this.el.nativeElement.offsetTop;
+    const scrollPosition = window.pageYOffset + 700;
+
+    if (scrollPosition >= componentPosition) {
+      this.state = 'scrolled';
+    } else {
+      this.state = 'normal';
+    }
+  }
 
   ngOnInit(): void {}
 
--- src/app/pages/home/components/third-section/third-section.component.ts
+++ src/app/pages/home/components/third-section/third-section.component.ts
@@ -1,16 +1,57 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ElementRef,
+  HostListener,
+} from '@angular/core';
 
 @Component({
   selector: 'app-third-section',
   templateUrl: './third-section.component.html',
   styleUrls: ['./third-section.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('scrollAnimation', [
+      state(
+        'scrolled',
+        style({
+          opacity: 1,
+        })
+      ),
+      state(
+        'normal',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition('scrolled => normal', animate('500ms ease-out')),
+      transition('normal => scrolled', animate('250ms ease-in')),
+    ]),
+  ],
 })
 export class ThirdSectionComponent implements OnInit {
+  state = 'normal';
 
-  constructor() { }
+  constructor(public el: ElementRef) {}
 
-  ngOnInit(): void {
+  @HostListener('window:scroll', ['$event'])
+  checkScroll(): void {
+    const componentPosition = this.el.nativeElement.offsetTop;
+    const scrollPosition = window.pageYOffset + 700;
+
+    if (scrollPosition >= componentPosition) {
+      this.state = 'scrolled';
+    } else {
+      this.state = 'normal';
+    }
   }
-
+  ngOnInit(): void {}
 }
