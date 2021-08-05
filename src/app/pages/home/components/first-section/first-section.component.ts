04d953cbb0e155a2e1ecc7dc933d5e5ee3c44530
Carlos San Bartolomé <carlos.sanbs@gmail.com>
2021-08-04 21:15:43 -0500

    DONE add navbar follow in scroll in home page

--- src/app/pages/home/components/first-section/first-section.component.ts
+++ src/app/pages/home/components/first-section/first-section.component.ts
@@ -1,8 +1,16 @@
 import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import {
   Component,
   OnInit,
   ChangeDetectionStrategy,
   ElementRef,
+  HostListener,
 } from '@angular/core';
 
 @Component({
@@ -10,9 +18,27 @@ import {
   templateUrl: './first-section.component.html',
   styleUrls: ['./first-section.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('fade', [
+      state('void', style({ opacity: 0 })),
+      transition(':enter', [animate(300)]),
+      transition(':leave', [animate(500)]),
+    ]),
+  ],
 })
 export class FirstSectionComponent implements OnInit {
   constructor(private elementRef: ElementRef) {}
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e: any): void {
+    if (window.pageYOffset > 150) {
+      const element = document.getElementById('navbar');
+      element.classList.add('sticky');
+    } else {
+      const element = document.getElementById('navbar');
+      element.classList.remove('sticky');
+    }
+  }
 
   ngOnInit(): void {
     const video = this.elementRef.nativeElement.querySelector(`#video`);
