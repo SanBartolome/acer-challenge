01e4d1cbe154279450211c7fd93eb6044b0464b2
Carlos San Bartolomé <carlos.sanbs@gmail.com>
2022-01-26 23:41:15 -0500

    DONE change laptops image size in live component and add sticky navbar

--- src/app/pages/live/live.component.ts
+++ src/app/pages/live/live.component.ts
@@ -1,14 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostListener,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-live',
   templateUrl: './live.component.html',
   styleUrls: ['./live.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('fade', [
+      state('void', style({ opacity: 0 })),
+      transition(':enter', [animate(300)]),
+      transition(':leave', [animate(500)]),
+    ]),
+  ],
 })
 export class LiveComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e: any): void {
+    if (window.pageYOffset > 150) {
+      const element = document.getElementById('navbar');
+      element.classList.add('sticky');
+    } else {
+      const element = document.getElementById('navbar');
+      element.classList.remove('sticky');
+    }
+  }
 
   openLiveChannel(): void {
     window.open('https://www.facebook.com/Solicorn1506');
